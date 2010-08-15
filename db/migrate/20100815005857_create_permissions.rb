ccf2749192e71c023fa265fbf7357cc0136486f9
AkitaOnRails <fabioakita@gmail.com>
2010-08-14 21:59:09 -0300

    adicionando permission

---
+++ db/migrate/20100815005857_create_permissions.rb
@@ -0,0 +1,13 @@
+class CreatePermissions < ActiveRecord::Migration
+  def self.up
+    create_table :permissions do |t|
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :permissions
+  end
+end
